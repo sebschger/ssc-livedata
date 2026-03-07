be0f4ba4e0f7a8e0e8b99b486eb058079c5bfa2c
sebschger <accounts@sebastianscharnagl.com>
2026-03-07 18:35:19 +0100

    Rounded off initial inaccuracies

--- sensor-data-pusher/main.py
+++ sensor-data-pusher/main.py
@@ -14,26 +14,35 @@ from flask import Flask, request
 from queue import Queue
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
+import os
 
 
 # Settings
 # todo: Hier arbeiten wir demnächst mit environment-variablen
-DATA_MESSAGE_LIST_NAME = "rx.metadata" # Hier wird definiert, wo die Metadaten des Payloads liegen
-DB_TABLE_NAME = "tabellenname" # todo: hier muss noch die env-variable rein
-DB_NAME = "dbname" # todo: hier die env-variable…
-DB_USER = "user" # todo: hier muss noch die env-variable rein
-DB_HOST = "host"  # todo: hier muss noch die env-variable rein
-DB_PASS = "dbpass" # todo: hier muss noch die env-variable rein
+DATA_MESSAGE_LIST_NAME = (
+    "rx.metadata"  # Hier wird definiert, wo die Metadaten des Payloads liegen
+)
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_TABLE = os.getenv("DB_TABLE")
+DB_USER = os.getenv("DB_USER")
+DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST")
+DB_PASS = os.getenv("DB_PASS")
+API_PORT = os.getenv("API_PORT")
+API_WEBHOOKNAME = os.getenv("API_WEBHOOKNAME")
 
 # Globale Variablen
 data_queue = Queue()
-sql_connection_string = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
-sql_connection = create_engine(sql_connection_string)
+sql_connection_string = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+sql_connection = create_engine(sql_connection_string, pool_pre_ping=True)
 
 listener = Flask(__name__)
 
-processing_thread = threading.Thread(target=interpret_message, daemon=True)
 
 def interpret_message():
     # hier ist die grundsätzliche Datenverarbeitung
@@ -41,15 +50,22 @@ def interpret_message():
 
     while True:
         item = data_queue.get()
-        publish_sql(clean_data(item))
-        data_queue.task_done()
+        try:
+            publish_sql(clean_data(item))
+        except Exception as e:
+            print("Problem beim Parsen der Daten:")
+            print(item)
+        finally:
+            data_queue.task_done()
 
 
+processing_thread = threading.Thread(target=interpret_message, daemon=True)
 
-@listener.route("/", methods=["POST"])
+
+@listener.route(API_WEBHOOKNAME, methods=["POST"])
 def handle_incoming():
     data_queue.put(request.get_json())
-    return 200, "OK"
+    return "OK", 200
 
 
 def clean_data(item: dict):
@@ -60,27 +76,28 @@ def clean_data(item: dict):
     # In diesem DF wird aber eine Spalte mite einer Liste von JSON-Strings sein
     # Das sind die Metadaten. Von denen nehmen wir nur die ersten
 
-    metadata_text = df_raw[DATA_MESSAGE_LIST_NAME].apply(lambda x: x[0])
-    metadata = json.loads(metadata_text)
-    df_metadata = pd.json_normalize(metadata)
+    if DATA_MESSAGE_LIST_NAME in df_raw.columns:
+        metadata = df_raw[DATA_MESSAGE_LIST_NAME].apply(lambda x: x[0] if x else [])
 
-    df_cleaned = pd.concat(
-        [
-            df_raw.drop(columns=[DATA_MESSAGE_LIST_NAME]),
-            df_metadata
-        ],
-        axis=1
-    ) 
+        df_metadata = pd.json_normalize(metadata)
+        df_metadata.index = df_raw.index
+        df_metadata.columns = DATA_MESSAGE_LIST_NAME + "." + df_metadata.columns
+
+        df_cleaned = pd.concat(
+            [df_raw.drop(columns=[DATA_MESSAGE_LIST_NAME]), df_metadata], axis=1
+        )
+
+    else:
+        df_cleaned = df_raw
 
     return df_cleaned
 
 
 def publish_sql(df_submit: pd.DataFrame):
     try:
-        df_submit.to_sql(DB_TABLE_NAME, con=sql_connection, if_exists="append")
+        df_submit.to_sql(DB_TABLE, con=sql_connection, if_exists="append")
     except Exception as e:
         print("Appending the incoming data to the mysql table failed")
-
 
 
 def main():
@@ -88,8 +105,14 @@ def main():
     processing_thread.start()
 
     # Wir starten den Server
-    waitress.serve(listener)
+    try:
+        waitress.serve(listener, port=int(API_PORT), host="0.0.0.0")
+    except Exception as e:
+        print(f"Problem mit dem Waitress-Prozess (Server):\n{e}")
+    finally:
+        # Wir sollten aber warten, bis alle Queue-Objekte abgearbeitet sind
+        data_queue.join()
 
-    # processing_thread.join() ist nicht nötig
-    # weil der Prozess in einer Endlosschleife ist 
-    # und daher nicht zuende geht
+
+if __name__ == "__main__":
+    main()
