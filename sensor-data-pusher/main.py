bd15403a94f7c8c99b931dbe7b430b5b5093ad0a
sebschger <accounts@sebastianscharnagl.com>
2026-03-08 09:40:12 +0100

    Improved data cleaning in the pusher

--- sensor-data-pusher/main.py
+++ sensor-data-pusher/main.py
@@ -6,22 +6,18 @@
 
 # Library Imports
 import pandas as pd
-import requests
 import threading
 import waitress
-import json
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 from queue import Queue
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
+import time
 
 
 # Settings
 # todo: Hier arbeiten wir demnächst mit environment-variablen
-DATA_MESSAGE_LIST_NAME = (
-    "rx.metadata"  # Hier wird definiert, wo die Metadaten des Payloads liegen
-)
 
 load_dotenv()
 
@@ -33,6 +29,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PASS = os.getenv("DB_PASS")
 API_PORT = os.getenv("API_PORT")
 API_WEBHOOKNAME = os.getenv("API_WEBHOOKNAME")
+DATA_MESSAGE_LIST_NAME = os.getenv("DATA_MESSAGE_LIST_NAME")
+
+def timestamp():
+    return time.strftime("%Y-%m-%d %H:%M:%s")
 
 # Globale Variablen
 data_queue = Queue()
@@ -42,6 +42,7 @@ sql_connection_string = (
 sql_connection = create_engine(sql_connection_string, pool_pre_ping=True)
 
 listener = Flask(__name__)
+bp = Blueprint("snsr-dt-pshr", __name__)
 
 
 def interpret_message():
@@ -53,7 +54,7 @@ def interpret_message():
         try:
             publish_sql(clean_data(item))
         except Exception as e:
-            print("Problem beim Parsen der Daten:")
+            print(timestamp(), "Problem beim Parsen der Daten:\n")
             print(item)
         finally:
             data_queue.task_done()
@@ -62,10 +63,21 @@ def interpret_message():
 processing_thread = threading.Thread(target=interpret_message, daemon=True)
 
 
-@listener.route(API_WEBHOOKNAME, methods=["POST"])
+@bp.route(API_WEBHOOKNAME, methods=["POST"])
 def handle_incoming():
+    print(timestamp(), "Data was received.")
     data_queue.put(request.get_json())
     return "OK", 200
+
+
+# Für den Health Check
+@bp.route("/", methods=["GET"])
+def answer_health_check():
+    print(timestamp(), "Probably: Health check received")
+    return "OK", 200
+
+
+listener.register_blueprint(bp, url_prefix="/sensor-data-pusher")
 
 
 def clean_data(item: dict):
@@ -76,19 +88,33 @@ def clean_data(item: dict):
     # In diesem DF wird aber eine Spalte mite einer Liste von JSON-Strings sein
     # Das sind die Metadaten. Von denen nehmen wir nur die ersten
 
-    if DATA_MESSAGE_LIST_NAME in df_raw.columns:
-        metadata = df_raw[DATA_MESSAGE_LIST_NAME].apply(lambda x: x[0] if x else [])
+    metadata_column = next(
+        (
+            column
+            for column in df_raw.columns.to_list()
+            if DATA_MESSAGE_LIST_NAME in column
+        ),
+        None,
+    )
+
+    if metadata_column in df_raw.columns:
+        metadata = df_raw[metadata_column].apply(lambda x: x[0] if x else [])
 
         df_metadata = pd.json_normalize(metadata)
         df_metadata.index = df_raw.index
-        df_metadata.columns = DATA_MESSAGE_LIST_NAME + "." + df_metadata.columns
+        df_metadata.columns = metadata_column + "." + df_metadata.columns
 
         df_cleaned = pd.concat(
-            [df_raw.drop(columns=[DATA_MESSAGE_LIST_NAME]), df_metadata], axis=1
+            [df_raw.drop(columns=[metadata_column]), df_metadata], axis=1
         )
 
     else:
         df_cleaned = df_raw
+
+    # Liste in Correlation_Ids in einen String umwandeln
+    df_cleaned["correlation_ids"] = df_cleaned["correlation_ids"].apply(
+        lambda x: "; ".join(x)
+    )
 
     return df_cleaned
 
@@ -97,7 +123,7 @@ def publish_sql(df_submit: pd.DataFrame):
     try:
         df_submit.to_sql(DB_TABLE, con=sql_connection, if_exists="append")
     except Exception as e:
-        print("Appending the incoming data to the mysql table failed")
+        print(timestamp(), f"Appending the incoming data to the mysql table failed:\n\n{e}")
 
 
 def main():
@@ -108,7 +134,7 @@ def main():
     try:
         waitress.serve(listener, port=int(API_PORT), host="0.0.0.0")
     except Exception as e:
-        print(f"Problem mit dem Waitress-Prozess (Server):\n{e}")
+        print(timestamp(), f"Problem mit dem Waitress-Prozess (Server):\n{e}")
     finally:
         # Wir sollten aber warten, bis alle Queue-Objekte abgearbeitet sind
         data_queue.join()
